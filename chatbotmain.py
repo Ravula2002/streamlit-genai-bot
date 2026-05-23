8fe39118c1dce2c165ff0a201d0f2c761fa8d23e
Ravula Praveen <134263253+Ravula2002@users.noreply.github.com>
2026-05-23 16:46:46 +0530

    initial commit

---
+++ chatbotmain.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+import streamlit as st
+
+
+load_dotenv()
+
+st.set_page_config(
+    page_title="Chat Bot",
+    page_icon=" ",
+    layout="centered",
+)
+st.title("GEN AI CHATBOT...")
+
+#inititating chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history=[]
+for msg in st.session_state.chat_history:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
+llm = ChatGroq(
+    model = "llama-3.3-70b-versatile"
+)
+
+user_input = st.chat_input("Ask hear...")
+if user_input:
+    st.chat_message("user").markdown(user_input)
+    st.session_state.chat_history.append({"role":"user","content":user_input})
+
+    responce = llm.invoke(input=[{"role":"assistant","content":"you are a helpful assistant, your name is bola"},*st.session_state.chat_history])
+    bot_replay = responce.content
+
+    st.session_state.chat_history.append({"role":"system","content":bot_replay})
+    with st.chat_message("system"):
+        st.markdown(bot_replay)
